4a755a506184b4bcf9ffd154ba70fccc6b538eca
Ruby Zhao <105169809+RubyVV@users.noreply.github.com>
2022-10-17 11:20:01 -0700

    Update multi_thread_server.py

--- multi_thread_server.py
+++ multi_thread_server.py
@@ -4,10 +4,11 @@ from threading import Thread
 # Multithreaded Python server : TCP Server Socket Thread Pool
 class ClientThread(Thread): 
  
-    def __init__(self,ip,port): 
+    def __init__(self,ip,port,conn): 
         Thread.__init__(self) 
         self.ip = ip 
-        self.port = port 
+        self.port = port
+        self.conn = conn
         print(f"[+] New server socket thread started for ip:{ip} and port: {port}")
  
     def run(self): 
@@ -16,7 +17,7 @@ class ClientThread(Thread):
             print(f"Server received data: {data}")
             if not data:
                 break
-            conn.send(data)  # echo 
+            self.conn.send(data) 
 
 # Multithreaded Python server : TCP Server Socket Program Stub
 TCP_IP = '127.0.0.1' 
@@ -32,6 +33,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.listen() 
         print("Multithreaded Python server : Waiting for connections from TCP clients...")
         (conn, (ip,port)) = s.accept() 
-        newthread = ClientThread(ip,port) 
+        newthread = ClientThread(ip,port,conn) 
         newthread.start() 
-        newthread.join()
+        
