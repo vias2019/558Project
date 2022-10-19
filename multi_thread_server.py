eed44c8213c5503bde307dfa29e71e3210d6861d
vias2019 <48966517+vias2019@users.noreply.github.com>
2022-10-18 17:15:19 -0700

    Add files via upload

--- multi_thread_server.py
+++ multi_thread_server.py
@@ -1,29 +1,45 @@
 import socket 
 from threading import Thread 
+from threading import Lock
+import dictionary
 
 # Multithreaded Python server : TCP Server Socket Thread Pool
 class ClientThread(Thread): 
- 
+    lock = Lock()
     def __init__(self,ip,port,conn): 
         Thread.__init__(self) 
         self.ip = ip 
         self.port = port
         self.conn = conn
         print(f"[+] New server socket thread started for ip:{ip} and port: {port}")
- 
+    
+    def translate (self, str):
+        object = dictionary.Translator()
+        return(object.searchDictionary(str))
+    
     def run(self): 
         while True : 
             data = conn.recv(2048) 
             print(f"Server received data: {data}")
             if not data:
                 break
-# You can uncomment the following two line to elongate the process and see the effect of multithreading
-#             for _ in range(50000000):
-#                 a = 2 ** 100
-            self.conn.send(data) 
+            with self.lock:
+#You can uncomment the following two line to elongate the process and see the effect of multithreading
+                # for _ in range(50000000):
+                #     a = 2 ** 100
+                    t = (data.decode('ascii')).lower()
+                    print(t)
+                    temp=self.translate(t)
+                    data=temp.encode('ascii')
+                    self.conn.send(data) 
+    
+    # def translate (str):
+    #     object = dictionary.Translator()
+    #     return(object.searchDictionary(str))
+           
 
 # Multithreaded Python server : TCP Server Socket Program Stub
-TCP_IP = '127.0.0.1' 
+TCP_IP = 'localhost' 
 TCP_PORT = 65432
 BUFFER_SIZE = 1024  # Usually 1024, but we need quick response 
 
